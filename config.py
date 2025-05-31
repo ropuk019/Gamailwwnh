8ecf2fcd3dfa40a8eb0f5cfb4b4273a70099d929
ropuk019 <125181975+ropuk019@users.noreply.github.com>
2025-05-31 13:12:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Bot token from BotFather
-TOKEN = '7911198465:AAFzYGLm0Pe_DICKYOXg2Tsyc27s6d933GQ'
-
-# Admin details (your account)
-ADMIN_ID = 7293015189
+TOKEN = '7911198465:AAFzYGLm0Pe_DICKYOXg2Tsyc27s6d933GQ'  # Must match bot.json's bot_token
+ADMIN_ID = 7293015189     # Your Telegram ID
 ADMIN_USERNAME = 'Prisoner7_7'
